0e284b3533eaf2fead13ab7233ce92c94e1b3b92
Jose JVB <bolufer@gmail.com>
2024-09-16 18:49:30 +0200

    updates

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase:{
+        apiKey: "AIzaSyA0pmjh8yE8bh2w4AgkcCn1z0bFBnAKw2o",
+        authDomain: "contactos-88875.firebaseapp.com",
+        projectId: "contactos-88875",
+        storageBucket: "contactos-88875.appspot.com",
+        messagingSenderId: "136492055703",
+        appId: "1:136492055703:web:57b2eedb6b48c453cffdd2"
+        },
+
+    }
+
